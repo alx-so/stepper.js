ab46591cbd5f653f88125abc27abdddbcc3dd640
alx-so <sokolenko.alexandr@gmail.com>
2021-01-03 22:42:16 +0200

    add: eventName enum

---
+++ src/events.ts
@@ -0,0 +1,15 @@
+enum EventName {
+    beforeSetup = 'stepper.before.setup',
+    afterSetup = 'stepper.after.setup',
+
+    beforeDestroy = 'stepper.before.destroy',
+    afterDestroy = 'stepper.after.destroy',
+
+    beforeReset = 'stepper.before.reset',
+    afterReset = 'stepper.after.reset',
+
+    beforeChange = 'stepper.before.change',
+    afterChange = 'stepper.after.change',
+}
+
+export default EventName;
