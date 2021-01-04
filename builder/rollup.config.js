bca20d5309ff972214d2b07b10770b2dd47294bb
alx-so <sokolenko.alexandr@gmail.com>
2021-01-04 10:51:28 +0200

    add: rollup yargs and dist mode

--- builder/rollup.config.js
+++ builder/rollup.config.js
@@ -1,5 +1,11 @@
 import typescript from '@rollup/plugin-typescript';
 import { terser } from "rollup-plugin-terser";
+import yargs from 'yargs/yargs';
+import { hideBin } from 'yargs/helpers';
+
+const argv = yargs(hideBin(process.argv)).argv;
+const isDist = argv['dist'];
+const plugins = composePlugins(isDist);
 
 export default {
     input: 'src/index.ts',
@@ -8,8 +14,14 @@ export default {
         format: 'umd',
         name: 'Stepper'
     },
-    plugins: [
-        typescript(),
-        // terser({ output: { comments: false } })
-    ]
+    plugins
 };
+
+function composePlugins(isDist) {
+    const def = [typescript()];
+    let plugins = [...def];
+
+    if (isDist) plugins = [...plugins, terser({ output: { comments: false } })];
+
+    return plugins;
+}
